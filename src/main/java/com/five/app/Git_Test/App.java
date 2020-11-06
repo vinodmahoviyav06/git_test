2cc4f33fee570d80bf1db695b8cb9af56824422b
vinodmahoviyav06 <vinodmahoviyav06@gmail.com>
2020-11-06 07:39:36 +0530

    gitTest project added to local reposotory

---
+++ src/main/java/com/five/app/Git_Test/App.java
@@ -0,0 +1,18 @@
+package com.five.app.Git_Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public void addData(String data) {
+		System.out.println("Data: "+data);
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
